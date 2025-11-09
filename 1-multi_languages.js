f4531aad7288a16892439d16cc3b3384c1abefcc
Stephen Maldima <maldima00@gmail.com>
2025-11-09 17:14:34 +0200

    add a new file with 3 sentences

---
+++ 1-multi_languages.js
@@ -0,0 +1,19 @@
+// 1. 3 languages
+
+// Write a script that prints 3 lines:
+
+// The first line: “C is fun”
+// The second line: “Python is cool”
+// The third line: “JavaScript is amazing”
+// You must use console.log(...) to print all output
+// You are not allowed to use var
+
+// solution
+
+let firstLine = 'C is fun';
+let secondLine = 'Python is cool';
+let thirdLine = 'JavaScript is amazing';
+
+console.log(firstLine);
+console.log(secondLine);
+console.log(thirdLine);
